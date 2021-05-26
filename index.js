5247a093e594772edc316dac9ef4299129126fb9
DKendle <dina.kendle@gmail.com>
2021-05-26 09:21:31 -0400

    All tests passing and code shows in document

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let m = document.querySelector("main")
+m.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Dina is the champion"
+
+document.body.appendChild(newHeader)
+
